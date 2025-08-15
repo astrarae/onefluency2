b57c7c15ccde037510660bbb6225f2de12508525
astrapc <astrarae.stelr@outlook.com>
2025-08-15 13:15:48 +0500

    Merge remote-tracking branch 'origin/main'

--- src/components/SparklesScreen.tsx
+++ src/components/SparklesScreen.tsx
@@ -1,7 +1,15 @@
 "use client";
 import { SparklesCore } from "./ui/Sparkles";
+import type {ReactNode} from "react";
 
-export function SparklesPreview({children}: any) {
+interface SparklesPreviewProps {
+    // Use ReactNode for children to accept any renderable content
+    children: ReactNode;
+}
+
+
+export function SparklesPreview({children}: SparklesPreviewProps) {
+
     return (
         <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
             <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
