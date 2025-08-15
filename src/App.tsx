c70c8d47e0eeec9436371a379d8be2362e88d98c
astrarae <astrarae.stelr@outlook.com>
2025-08-15 09:40:03 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+import {SparklesPreview} from "@/components/SparklesScreen.tsx";
+
+
+function App() {
+  return (
+      <SparklesPreview></SparklesPreview>
+
+  )
+}
+
+export default App
