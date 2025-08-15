a01a9dad1d97d165b31b8fa49605f6f58285a37a
astrapc <astrarae.stelr@outlook.com>
2025-08-15 10:19:37 +0500

    dsa

--- src/components/SparklesScreen.tsx
+++ src/components/SparklesScreen.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { SparklesCore } from "./ui/Sparkles";
 
-export function SparklesPreview() {
+export function SparklesPreview({children}) {
     return (
         <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
             <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
@@ -13,6 +13,7 @@ export function SparklesPreview() {
                 <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
                 <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
                 <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
 
                 {/* Core component */}
                 <SparklesCore
@@ -26,6 +27,7 @@ export function SparklesPreview() {
 
                 {/* Radial Gradient to prevent sharp edges */}
                 <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+                {children}
             </div>
         </div>
     );
